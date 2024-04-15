ecff4d615a18350adb7bce5d5a8df7f2b7019fe6
host <2257699870@qq.com>
2024-04-15 11:28:07 +0800

    none

--- jupyter三次方程.py
+++ jupyter三次方程.py
@@ -1,10 +1,19 @@
-print("一元三次方程的基本形式:aX³+bX²+cX+d=0(其中a,b,c,d为常数）")
+#! /user/bin/python3
+
 #  Copyright (c) 2024. All rights reserved.
 #  This source code is licensed under the CC BY-NC-ND
 #  (Creative Commons Attribution-NonCommercial-NoDerivatives) License, By Xiao Songtao.
 #  This software is protected by copyright law. Reproduction, distribution, or use for commercial
 #  purposes is prohibited without the author's permission. If you have any questions or require
 #  permission, please contact the author: 2207150234@st.sziit.edu.cn
+
+# -------------------------<Lenovo>----------------------------
+# 传建时间: 2023/10/3 13:14
+# 当前项目名: python
+# 编码模式: utf-8
+# 注释:
+# -------------------------<Lenovo>----------------------------
+print("一元三次方程的基本形式:aX³+bX²+cX+d=0(其中a,b,c,d为常数）")
 
 from fractions import Fraction
 import numpy as np
